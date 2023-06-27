1d05e1838fc96aa8073be9bd507a25b3c784b695
Connor Hindley <connor.hindley@tanium.com>
2023-06-27 16:21:28 -0600

    Add http import.
    Just finding the point where centos 5 breaks

--- main.go
+++ main.go
@@ -2,12 +2,32 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"net/http"
 	"os/exec"
 	"runtime"
 	"strings"
 )
 
 func main() {
-	out, _ := exec.Command("uname", "-a").CombinedOutput()
-	fmt.Printf("Hello, %s_%s!\nuname=%q\ngo=%q\n", runtime.GOOS, runtime.GOARCH, strings.TrimSpace(string(out)), runtime.Version())
+	fmt.Printf("Hello, %s_%s!\nuname=%q\ngo=%q\nip=%q\n",
+		runtime.GOOS, runtime.GOARCH, mustGetUnameOutput(), runtime.Version(), mustGetIpAddress(),
+	)
+}
+
+func mustGetUnameOutput() string {
+	out, err := exec.Command("uname", "-a").CombinedOutput()
+	if err != nil {
+		panic(err)
+	}
+	return strings.TrimSpace(string(out))
+}
+
+func mustGetIpAddress() string {
+	res, err := http.Get("https://api.ipify.org")
+	if err != nil {
+		panic(err)
+	}
+	ip, _ := io.ReadAll(res.Body)
+	return string(ip)
 }
